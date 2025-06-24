03b3f27db7a2d9df27166496c75dd3b4b017b846
Domenik Eichhorn <domenik.eichhorn@kit.edu>
2025-06-24 18:23:03 +0200

    added cnot and tcount

--- qasm_circuit_reader.py
+++ qasm_circuit_reader.py
@@ -6,14 +6,25 @@ circuit_path = "circuits_target_qiskit"
 
 # read open qasm circuits from directory:
 data = []
-data.append(['name', 'num_qubits', 'width', 'depth'])
+data.append(['name', 'num_qubits', 'width', 'depth', 'num_cnot', 'num_t'])
 for filename in os.listdir(circuit_path):
     path = os.path.join(circuit_path, filename)
     # interpret openqasm als qiskit QuantumCircuit
     circuit = qasm2.load(filename=path, 
         custom_instructions=qasm2.LEGACY_CUSTOM_INSTRUCTIONS)
     # add explanatory variables to data
-    data.append([filename, circuit.num_qubits, circuit.width(), circuit.depth()])
+    cnot_count = circuit.count_ops().get('cx') or 0
+    t_count = circuit.count_ops().get('t') or 0
+
+    data.append([filename,
+        circuit.num_qubits, 
+        circuit.width(), 
+        circuit.depth(), 
+        cnot_count,
+        t_count])
+    
+    print(circuit.count_ops())
+    exit()
 
 # write data in .csv file
 with open('circuit_data.csv', mode='w', newline='') as file:
