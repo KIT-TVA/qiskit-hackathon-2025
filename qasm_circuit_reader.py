39dca14a64401c1aaa48d6a458430e2b3a801973
Domenik Eichhorn <domenik.eichhorn@kit.edu>
2025-06-24 18:57:50 +0200

    added 2 and 3 qubit gate numbers

--- qasm_circuit_reader.py
+++ qasm_circuit_reader.py
@@ -2,11 +2,13 @@ import os
 import csv
 from qiskit import qasm2
 
+from collections import defaultdict
+
 circuit_path = "circuits_target_qiskit"
 
 # read open qasm circuits from directory:
 data = []
-data.append(['name', 'num_qubits', 'width', 'depth', 'num_cnot', 'num_t'])
+data.append(['name', 'num_qubits', 'width', 'depth', 'num_cnot', 'num_t', 'num_2qbit', 'num_3qbit'])
 for filename in os.listdir(circuit_path):
     path = os.path.join(circuit_path, filename)
     # interpret openqasm als qiskit QuantumCircuit
@@ -15,16 +17,20 @@ for filename in os.listdir(circuit_path):
     # add explanatory variables to data
     cnot_count = circuit.count_ops().get('cx') or 0
     t_count = circuit.count_ops().get('t') or 0
+    count_multi_qbit_gates = defaultdict(int)
+    for entry in circuit.data:
+        count_multi_qbit_gates[len(entry.qubits)] += 1
+    two_qbit_gate_count = count_multi_qbit_gates.get(2) or 0
+    three_qbit_gate_count = count_multi_qbit_gates.get(3) or 0
 
     data.append([filename,
         circuit.num_qubits, 
         circuit.width(), 
         circuit.depth(), 
         cnot_count,
-        t_count])
-    
-    print(circuit.count_ops())
-    exit()
+        t_count,
+        two_qbit_gate_count,
+        three_qbit_gate_count])
 
 # write data in .csv file
 with open('circuit_data.csv', mode='w', newline='') as file:
